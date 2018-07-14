45f292e48f6e89c16b8a75460b166b772af8db8f
AlanNggg <41237850+AlanNggg@users.noreply.github.com>
2018-07-14 23:48:18 +0800

    Update Login.php

--- Login.php
+++ Login.php
@@ -3,37 +3,60 @@
 
     extract($_POST);
 
-    $sql = "SELECT * FROM `managers`";
-            $result = mysqli_query($conn, $sql);
-            if (mysqli_num_rows($result) > 0) {
-                $table = <<< EOF
-                    <table>
-                        <tr>
-                            <th>ManagerId</th>
-                            <th>Name</th>
-                            <th>Password</th>
-                        </tr>
+    function goToControl($role, $username, $password, $name) {
+        $form = <<< EOF
+                    <form method="POST" action="control.php" id="goToControlForm">
+                        <input type="hidden" name="role" id="role" value="$role"/>
+                        <input type="hidden" name="name" id="name" value="$name"/>
+                        <input type="hidden" name="username" id ="username" value="$username"/>
+                        <input type="hidden" name="password" id ="password" value="$password"/>
+                    </form>
 EOF;
-                echo $table;
-            } 
+        echo $form;
+        ?>
+        
+        <script type="text/javascript">
+            document.getElementById("goToControlForm").submit();
+        </script>
+        
+        <?php
+    }
 
-    if (isset($_POST["username"], $_POST["password"], $_POST["role"])) {        
+    $sql = "SELECT * FROM `$role`";
+    $result = mysqli_query($conn, $sql);
+    $success = false;
+
+    if (isset($username, $password, $role)) {        
         
         switch ($role) {
             case "managers":
-
             while ($row = mysqli_fetch_assoc($result)) {
                 if ($row["ManagerId"] == $username && $row["Password"] == $password) {
-                    echo "<tr><th>".$row["ManagerId"]."</th>";
-                    echo "<th>".$row["Name"]."</th>";
-                    echo "<th>".$row["Password"]."</th></tr>";
+                    $name = $row["Name"];
+                    $success = true;
+                    goToControl($role, $username, $password, $name);
                 }
             }
+            
             break;
+
+            case "restaurants":
+            while ($row = mysqli_fetch_assoc($result)) {
+                if ($row["RestaurantId"] == $username && $row["Password"] == $password) {
+                    $name = $row["Name"];
+                    $success = true;
+                    goToControl($role, $username, $password, $name);
+                }
+            }
+            
+            break;
+
+            default:
+            break;
+        }          
+
+        if (!$success) {
+            header("Location: Login.html");
         }
-    
-        
-        
     }
-   
 ?>
