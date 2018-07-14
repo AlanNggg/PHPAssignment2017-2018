71a2eb7ed4a8380c1af9c9eabcc8f3d31b620fb1
TerryFunggg <41190384+TerryFunggg@users.noreply.github.com>
2018-07-14 23:02:12 +0800

    V4.2
    Done all the basic UI of Restaurant
    
    Co-Authored-By: terryfunggg <terryfunggg@users.noreply.github.com>

--- restaurant.php
+++ restaurant.php
@@ -21,8 +21,8 @@
 <body>
             <!-- set of function button -->
             <button class="circle1" href="#modal-container"uk-toggle></button>
-            <span class="circle2"></span>
-            <span class="circle3"></span>
+            <button class="circle2" href="#modal-WareHouse"uk-toggle></button>
+            <button class="circle3" href="#modal-Description"uk-toggle></button>
             <!-- function name -->
             <h1 class="fTitle1">Order</h1>
             <h1 class="fTitle2">WareHouse</h1>
@@ -75,8 +75,8 @@
                            
                         </tbody>
                     </table>
-                            </div>
-                    </div>
+                 </div>
+            </div>
 
                         <!-- Create Order Table -->
                         <div id="modal-createOrder" class="uk-modal-full" uk-modal>
@@ -87,62 +87,134 @@
                                 <button class="uk-modal-close-default" type="button" uk-close></button>
 
                                 <!-- set grid and width about the modal uk-child-width-1-2 is mean it moke to class in half  -->
-                                <div class="uk-grid-collapse uk-child-width-1-2@s uk-flex-middle" uk-grid> 
+                                
 
                                 <!-- The header text of modal -->
                                     <div class="uk-modal-header">
                                         <h2 class="uk-modal-title">Create Order</h2>
-                                    
-                                    <!-- Set the padding with all under element uk-padding is defualt padding in UIkit  -->
-                                     <div class="uk-padding">
-
-                                         <!-- Form -->
-                                     <form class="uk-form-stacked">
-                                        <!-- Restaurant ID info -->
-                                        <div class="uk-margin">
-                                            <label class="uk-form-label" for="form-stacked-text">Restaurant Id</label>
-                                            <div class="uk-form-controls">
-                                                <input class="uk-input" id="form-stacked-text" type="text" placeholder="Some text...">
-                                            </div>
-                                        </div>
-                                        <!-- Supplier Stock ID -->
-                                        <div class="uk-margin">
-                                            <label class="uk-form-label" for="form-stacked-text">SupplierStock Id</label>
-                                            <div class="uk-form-controls">
-                                                <input class="uk-input" id="form-stacked-text" type="text" placeholder="Some text...">
-                                            </div>
-                                        </div>
-
-                                        <div class="uk-margin">
-                                            <label class="uk-form-label" for="form-stacked-select">Select</label>
-                                            <div class="uk-form-controls">
-                                                <select class="uk-select" id="form-stacked-select">
-                                                    <option>Option 01</option>
-                                                    <option>Option 02</option>
-                                                </select>
-                                            </div>
-                                        </div>
-
-                                        <div class="uk-margin">
-                                            <div class="uk-form-label">Radio</div>
-                                            <div class="uk-form-controls">
-                                                <label><input class="uk-radio" type="radio" name="radio1"> Option 01</label><br>
-                                                <label><input class="uk-radio" type="radio" name="radio1"> Option 02</label>
-                                            </div>
-                                        </div>
-
-                                     </form>
-                                 </div>
-                                 
-                                    <!-- The button set of the modal page -->
-                                     <div class="uk-modal-footer uk-text-right">
-                                        <button class="uk-button uk-button-default uk-modal-close" type="button">Cancel</button>
-                                        <button class="uk-button uk-button-primary" type="button">Save</button>
                                     </div>
-                                </div>
-                            </div>
-                            </div>
+                                    <!-- Set the padding with all under element uk-padding is defualt padding in UIkit  -->
+                                    <div class="uk-padding">
+                                        <form class="uk-grid-small" uk-grid>
+
+                                            <div class="uk-grid-collapse uk-child-width-1-2@s uk-flex-middle" uk-grid>
+                                                    <div class="uk-width-1-2">
+                                                        Order ID:
+                                                        <input class="uk-input uk-form-width-medium" type="text" placeholder="100">
+                                                    </div>
+                                                    <div class="uk-width-1-2">
+                                                         Order ID:
+                                                        <input class="uk-input uk-form-width-medium" type="text" placeholder="100">
+                                                    </div>
+
+                                             </div>
+
+                                            <div class="uk-width-1-1">
+                                                Order ID:
+                                                <input class="uk-input uk-form-width-medium" type="text" placeholder="100">
+                                            </div>
+                                       
+                                        
+
+                                            <table class="uk-table uk-table-hover uk-table-divider">
+                                                <thead>
+                                                    <tr>
+                                                        <th>OrderId</th>
+                                                        <th>Supplier Name</th>
+                                                        <th>Stock Name</th>
+                                                        <th>Order Amount</th>
+                                                        <th>Purchase Date</th>
+                                                        <th>Delivery Date</th>
+                                                        <th>Received Date</th>
+                                                        <th></th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr>
+                                                        <td>Table Data</td>
+                                                        <td>Table Data</td>
+                                                        <td>Table Data</td>
+                                                        <td>Table Data</td>
+                                                        <td>Table Data</td>
+                                                        <td>Table Data</td>
+                                                        <td>Table Data</td>
+                                                        <td><button class="uk-button uk-button-danger" type="button">Delete</button></td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </form>
+                                     </div>   
+                             </div>
                         </div>
+
+
+                <!-- Function of wareHouse -->
+                <div id="modal-WareHouse" class="uk-modal-container" uk-modal>
+               
+                    <div class="uk-modal-dialog uk-modal-body">
+
+                   <button class="uk-modal-close-default" type="button" uk-close></button>
+
+                   <h2 class="uk-modal-title">WareHouse</h2>
+                  
+
+                   <!-- Show the  Item table -->
+                   <table class="uk-table uk-table-hover uk-table-divider">
+                       <thead>
+                           <tr>
+                               <th>Stock ID</th>
+                               <th>Stock Name</th>
+                               <th>Amount</th>
+                               
+                           </tr>
+                       </thead>
+                       <tbody>
+                           <tr>
+                               <td>Table Data</td>
+                               <td>Table Data</td>
+                               <td>Table Data</td>
+                               
+                           </tr>
+                           
+                          
+                       </tbody>
+                    </table>
+                    </div>
+                 </div>
+
+                 <div id="modal-Description" uk-modal>
+               
+               <div class="uk-modal-dialog uk-modal-body">
+
+              <button class="uk-modal-close-default" type="button" uk-close></button>
+
+              <h2 class="uk-modal-title">Restaurant Description</h2>
+             
+              <form>
+                    <fieldset class="uk-fieldset">
+
+                        <div class="uk-margin">
+                             Restaurant Name:
+                            <input class="uk-input" type="text" placeholder="Input">
+                        </div>
+
+                        
+
+                        <div class="uk-margin">
+                            Description:
+                            <textarea class="uk-textarea" rows="5" placeholder="Textarea"></textarea>
+                        </div>
+
+                        <input class="uk-input uk-button uk-button-primary uk-width-1-2" type="submit" placeholder="Input">
+
+                        
+
+                    </fieldset>
+                </form>
+
+             
+               </div>
+            </div>
                         
 
 </body>
